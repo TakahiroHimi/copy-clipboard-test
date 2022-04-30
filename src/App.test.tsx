76b9be08fea326ccda7faac0c9f57922f1a077bd
TakahiroHimi <takahiro.mth.1209@gmail.com>
2022-04-30 22:24:51 +0900

    test作成

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+beforeEach(() => {
+  Object.defineProperty(window.navigator, "clipboard", {
+    value: {
+      item: "",
+      readText() {
+        if (typeof this.item !== "string") return Promise.resolve("");
+        return Promise.resolve(this.item);
+      },
+      writeText(text: string) {
+        this.item = text;
+        return Promise.resolve();
+      },
+    },
+  });
+});
+
+test("copy clipboard", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const copyText = await window.navigator.clipboard.readText();
+
+  expect(copyText).toBe("hoge");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from "react";
 import "./App.css";
-import copy from "copy-to-clipboard";
 
 function App() {
   return (
     <>
-      <button onClick={() => copy("hoge")}>hoge</button>
+      <button onClick={() => window.navigator.clipboard.writeText("hoge")}>
+        copy hoge
+      </button>
+      {/* <button onClick={() => copyImage()}>copy hoge</button> */}
     </>
   );
 }
 
 export default App;
+
+const copyImage = async () => {
+  const res = await fetch(
+    "https://w7.pngwing.com/pngs/107/759/png-transparent-circle-white-circle-white-monochrome-black-thumbnail.png"
+  );
+  const blob = await res.blob();
+  const item = new ClipboardItem({
+    "image/png": blob,
+  });
+  await navigator.clipboard.write([item]);
+  window.navigator.clipboard
+    .readText()
+    .then(
+      (value) => console.log("ok", value),
+      (reason) => console.log("ng", reason)
+    )
+    .catch((reason) => console.log("error", reason));
+};
